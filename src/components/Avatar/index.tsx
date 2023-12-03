9c6d128a6457b303c32caa699eaf3123bd5ca204
Gleb <g.elenev@gmail.com>
2023-12-03 17:01:08 +0300

    refactor: tune style

--- src/components/Avatar/index.tsx
+++ src/components/Avatar/index.tsx
@@ -3,25 +3,29 @@ import s from './Avatar.module.scss';
 export function Avatar() {
   return (
     <div className={s.wrapper}>
-      <p className={s.avatar}>АИ</p>
-      <h3 className={s.name}>Иван Иванов</h3>
-      <ul className={s.linksList}>
-        <li className={s.linkItem}>
-          <a href='#' target='_blank' className={s.link}>
-            Telegram
-          </a>
-        </li>
-        <li className={s.linkItem}>
-          <a href='#' target='_blank' className={s.link}>
-            Github
-          </a>
-        </li>
-        <li className={s.linkItem}>
-          <a href='#' target='_blank' className={s.link} rel='noreferrer'>
-            Resume
-          </a>
-        </li>
-      </ul>
+      <div className={s.avatar} role='image'>
+        АИ
+      </div>
+      <div className={s.data}>
+        <h3 className={s.name}>Иван Иванов</h3>
+        <ul className={s.linksList}>
+          <li className={s.linkItem}>
+            <a href='#' target='_blank' className={s.link}>
+              Telegram
+            </a>
+          </li>
+          <li className={s.linkItem}>
+            <a href='#' target='_blank' className={s.link}>
+              Github
+            </a>
+          </li>
+          <li className={s.linkItem}>
+            <a href='#' target='_blank' className={s.link} rel='noreferrer'>
+              Resume
+            </a>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 }
