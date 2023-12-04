2cc0c7b703bb3327a07c470dcd6e524d85350d95
Gleb <g.elenev@gmail.com>
2023-12-04 07:15:36 +0300

    refactor: mui components for styling

--- src/pages/CreatePage/FormSteps/FirstForm.tsx
+++ src/pages/CreatePage/FormSteps/FirstForm.tsx
@@ -1,7 +1,7 @@
 import { useFormContext } from 'react-hook-form';
 import { TextInput } from '@/components/Form';
 import { GENDERS } from '@/types';
-import { MenuItem } from '@mui/material';
+import { Grid, InputLabel, MenuItem } from '@mui/material';
 import { useAppDispatch } from '@/store/hooks';
 import { incrementStep } from '@/store/form.slice';
 import s from './FormSteps.module.scss';
@@ -16,28 +16,51 @@ export function FirstForm({ id }: { id: string }) {
 
   return (
     <form className={s.form} onSubmit={methods.handleSubmit(onSubmit)} id={id}>
-      <label className={s.label}>
-        NickName
-        <TextInput name={'nickname'} id={'user-nickname'} placeholder={'Enter nickname'} margin={'dense'} />
-      </label>
-      <label className={s.label}>
-        Name
-        <TextInput name={'name'} id={'user-name'} placeholder={'Enter name'} margin={'dense'} />
-      </label>
-      <label className={s.label}>
-        Surname
-        <TextInput name={'surname'} id={'user-surname'} placeholder={'Enter surname'} margin={'dense'} />
-      </label>
-      <label className={s.label}>
-        Sex
-        <TextInput name={'sex'} id={'field-sex'} margin={'dense'} select>
-          {GENDERS.map((item, i) => (
-            <MenuItem key={i} value={item} id={`field-sex-option-${item}`}>
-              {`${item[0].toUpperCase()}${item.substring(1)}`}
-            </MenuItem>
-          ))}
-        </TextInput>
-      </label>
+      <Grid container spacing={2} direction={'column'} marginBlockEnd={2}>
+        <Grid container item direction={'column'} md={6} xs={12}>
+          <InputLabel htmlFor='field-nickname'>NickName</InputLabel>
+          <TextInput
+            name={'nickname'}
+            size={'small'}
+            id={'field-nickname'}
+            placeholder={'Enter nickname'}
+            margin={'dense'}
+            style={{ maxWidth: 300 }}
+          />
+        </Grid>
+        <Grid container item direction={'column'} md={6} xs={12}>
+          <InputLabel htmlFor='field-name'>Name</InputLabel>
+          <TextInput
+            name={'name'}
+            size={'small'}
+            id={'field-name'}
+            placeholder={'Enter name'}
+            margin={'dense'}
+            style={{ maxWidth: 300 }}
+          />
+        </Grid>
+        <Grid container item direction={'column'} md={6} xs={12}>
+          <InputLabel htmlFor='field-surname'>Surname</InputLabel>
+          <TextInput
+            name={'surname'}
+            size={'small'}
+            id={'field-surname'}
+            placeholder={'Enter surname'}
+            margin={'dense'}
+            style={{ maxWidth: 300 }}
+          />
+        </Grid>
+        <Grid container item direction={'column'} md={6} xs={12}>
+          <InputLabel htmlFor='field-sex'>Sex</InputLabel>
+          <TextInput name={'sex'} size={'small'} id={'field-sex'} margin={'dense'} style={{ maxWidth: 300 }} select>
+            {GENDERS.map((item, i) => (
+              <MenuItem key={i} value={item} id={`field-sex-option-${item}`}>
+                {`${item[0].toUpperCase()}${item.substring(1)}`}
+              </MenuItem>
+            ))}
+          </TextInput>
+        </Grid>
+      </Grid>
     </form>
   );
 }
